8708d433a6218ce8d60b58a9f333c2f92ad7fd06
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2022-05-28 20:45:13 -0300

    Se crea el slider en galeria

---
+++ javascript/slider.js
@@ -0,0 +1,41 @@
+let indice = 1;
+muestraSlides(indice);
+
+
+// esta funciona el boton de siguiente
+function avanzaSlide(n){
+    muestraSlides( indice+=n );
+}
+
+// Esta funcion es para el selector de posición
+function posicionSlide(n){
+    muestraSlides(indice=n);
+}
+
+setInterval(function tiempo(){
+    muestraSlides(indice+=1)
+},4000);
+
+
+function muestraSlides(n){
+    let i;
+    let slides = document.getElementsByClassName('slider');
+    let barras = document.getElementsByClassName('barra');
+
+    if(n > slides.length){
+        indice = 1;
+    }
+    if(n < 1){
+        indice = slides.length;
+    }
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    for(i = 0; i < barras.length; i++){
+        barras[i].className = barras[i].className.replace(" active", "");
+    }
+
+    slides[indice-1].style.display = 'block';
+    barras[indice-1].className += ' active';
+
+}
