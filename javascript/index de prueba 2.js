a74297edb9183f960bb97c48da6003dd299cd0c5
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2022-05-27 21:01:19 -0300

    cambios minimos

---
+++ javascript/index de prueba 2.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const myform = document.querySelector('form')
+    const inputName = myform.querySelector('#name')
+    const inputEmail = myform.querySelector('#email')
+    const inputPhone = myform.querySelector('#phone')
+    const inputMessage = myform.querySelector('#message')
+    const submitButton = myform.querySelector('#submit')
+
+
+    
+    submitButton.addEventListener('click', (event) => {
+       
+        const contactInfo = {
+            name: inputName.value,
+            email: inputEmail.value,
+            phone: inputPhone.value,
+            message: inputMessage.value
+
+        }
+        return window.localStorage.setItem('contact', JSON.stringify(contactInfo))
+        // console.log(contactInfo)
+    })
+   
+})
