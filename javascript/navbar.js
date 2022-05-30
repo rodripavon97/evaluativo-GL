a3f8621c312e8864753ecdf66641672c4eb10fff
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2022-05-30 15:06:11 -0300

    Agrega js a navbar, renombra galeria a productos

---
+++ javascript/navbar.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const activePage = window.location.pathname;
+  const navLinks = document.querySelectorAll("nav a").forEach((link) => {
+    if (link.href.includes(`${activePage}`)) {
+      link.classList.add("active");
+    }
+  });
+  return navLinks
+});
