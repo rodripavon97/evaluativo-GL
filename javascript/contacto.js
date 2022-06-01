c13b9bb2e203a82d0baa1579d46dacbf7abd1cd9
Tomas Catalano <tomas.a.catalano@gmail.com>
2022-06-01 12:29:44 -0300

    todo corregido

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -3,43 +3,41 @@ window.addEventListener("DOMContentLoaded", () => {
         const activePage = window.location.pathname;
         const navLinks = document.querySelectorAll("nav a").forEach((link) => {
             if (link.href.includes(`${activePage}`)) {
-                link.classList.add("active");
+                link.classList.add("active")
             }
         });
     }
     
     function formSend() {
-        const myform = document.querySelector("form");
-        const inputName = myform.querySelector('#name');
-        const inputEmail = myform.querySelector("#email");
-    const inputPhone = myform.querySelector("#phone");
-    const inputMessage = myform.querySelector("#message");
-    const submitButton = myform.querySelector("#submit");
+        const myform = document.querySelector("form")
+        const inputName = myform.querySelector('#name')
+        const inputEmail = myform.querySelector("#email")
+        const inputPhone = myform.querySelector("#phone")
+        const inputMessage = myform.querySelector("#message")
+        const submitButton = myform.querySelector("#submit")
     
     submitButton.addEventListener("click", (event) => {
-        event.preventDefault();
-      const contactInfo = {
-        name: inputName.value,
-        email: inputEmail.value,
-        phone: inputPhone.value,
-        message: inputMessage.value,
-    };
-      console.log(contactInfo);
-      const savedData = JSON.parse(localStorage.getItem("contacts")) || [];
+        //event.preventDefault()
+       
+        const contactInfo = {
+            name: inputName.value,
+            email: inputEmail.value,
+            phone: inputPhone.value,
+            message: inputMessage.value,
+        }
+      
+        const savedData = JSON.parse(window.localStorage.getItem("contacts")) || []
       if (
-          contactInfo.name !== "" &&
+        contactInfo.name !== "" &&
         contactInfo.email !== "" &&
         contactInfo.phone !== "" &&
         contactInfo.message !== ""
         ) {
-            localStorage.setItem(
-          "contacts",
-          JSON.stringify([...savedData, contactInfo])
-        );
+            window.localStorage.setItem("contacts",JSON.stringify([...savedData, contactInfo]))
       } else {
-          alert("Por favor llena todos los campos");
+          alert("Por favor llena todos los campos")
       }
-    });
+    })
 }
 
 navbarActive();
